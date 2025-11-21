bbbf14fe0852654a311baeda87c246001195216d
rafaabc <faelsabc21@gmail.com>
2025-11-21 15:43:33 +0100

    Add fixtures to handle request body

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,13 +1,13 @@
 const request = require("supertest");
+const postLogin = require("../fixtures/postLogin.json");
 
 const getToken = async (username, password) => {
+  const bodyLogin = { ...postLogin };
+
   const responseLogin = await request(process.env.BASE_URL)
     .post("/login")
     .set("Content-Type", "application/json")
-    .send({
-      username: username,
-      senha: password,
-    });
+    .send(bodyLogin);
 
   return responseLogin.body.token;
 };
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const { expect } = require("chai");
 require("dotenv").config();
 const { getToken } = require("../helpers/autenticacao");
+const postTransferencias = require("../fixtures/postTransferencias.json");
 
 describe("Transferências", () => {
   describe("POST /transferencias", () => {
@@ -12,31 +13,26 @@ describe("Transferências", () => {
     });
 
     it("When sending a transfer greater or equal to R$ 10,00, then return 201", async () => {
+      const bodyTransferencias = { ...postTransferencias };
+
       const response = await request(process.env.BASE_URL)
         .post("/transferencias")
         .set("Content-Type", "application/json")
         .set("Authorization", `Bearer ${token}`)
-        .send({
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 10,
-          token: "",
-        });
+        .send(bodyTransferencias);
 
       expect(response.status).to.equal(201);
     });
 
     it("When sending a transfer less than R$ 10,00, then return 422", async () => {
+      const bodyTransferencias = { ...postTransferencias };
+      bodyTransferencias.valor = 9.99;
+
       const response = await request("http://localhost:3000")
         .post("/transferencias")
         .set("Content-Type", "application/json")
         .set("Authorization", `Bearer ${token}`)
-        .send({
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 9.99,
-          token: "",
-        });
+        .send(bodyTransferencias);
 
       expect(response.status).to.equal(422);
     });
